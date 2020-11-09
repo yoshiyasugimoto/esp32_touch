7a9920301a28ba8f9210b8115b8b2e6b7e96db12
Yoshiya Sugimoto <y.sugimoto@jxpress.net>
2020-11-10 07:32:39 +0900

    Create esp32 touchRead

--- serial_print/serial_print.ino
+++ serial_print/serial_print.ino
@@ -1,26 +1,5 @@
-//#include <CapacitiveSensor.h>
-//
-//#define LED_BUILTIN 
-//
-//CapacitiveSensor touchPad = CapacitiveSensor(4, 2);
-//
-//void setup() {
-//  pinMode(LED_BUILTIN, OUTPUT);
-//  touchPad.set_CS_AutocaL_Millis(0xFFFFFFFF);
-//  Serial.begin(115200);
-//}
-//
-//void loop() {
-//  long val = touchPad.capacitiveSensor(30);
-//  Serial.println(val);
-//
-//  delay(10);
-//}
-//
-
 // set pin numbers
-const int touchPin = 4; 
-const int ledPin = 16;
+const int touchPin = T0; 
 
 // change with your threshold value
 const int threshold = 20;
@@ -30,8 +9,6 @@ int touchValue;
 void setup(){
   Serial.begin(115200);
   delay(1000); // give me time to bring up serial monitor
-  // initialize the LED pin as an output:
-  pinMode (ledPin, OUTPUT);
 }
 
 void loop(){
@@ -42,25 +19,10 @@ void loop(){
   // if it is, set ledPin to HIGH
   if(touchValue < threshold){
     // turn LED on
-    digitalWrite(ledPin, HIGH);
     Serial.println(" - LED on");
   }
   else{
     // turn LED off
-    digitalWrite(ledPin, LOW);
     Serial.println(" - LED off");
   }
-  delay(500);
 }
-
-
-//void setup() {
-//  Serial.begin(115200);
-//  delay(1000); // give me time to bring up serial monitor
-//  Serial.println("ESP32 Touch Test");
-//}
-//
-//void loop() {
-//  Serial.println(touchRead(4));  // get value of Touch 0 pin = GPIO 4
-//  delay(1000);
-//}
